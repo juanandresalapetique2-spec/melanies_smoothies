5f635b903b9935ab44995717e704420a7e48486c
juanandresalapetique2-spec <juanandresalapetique2@gmail.com>
2026-03-03 17:05:35 -0500

    Refactor ingredient processing in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,11 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ' '
   for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
-        )
-        st.dataframe(
-            data=smoothiefroot_response.json(),
-            use_container_width=True
-        )
-
+    ingredients_string += fruit_chosen + ' '
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+    st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    
     my_insert_stmt = """ 
         insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """')
